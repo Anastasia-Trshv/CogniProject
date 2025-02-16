f79454e65f369377ca92ebe073838547e08b85aa
iv-develop <158550581+iv-develop@users.noreply.github.com>
2025-02-17 00:25:25 +0300

    friends api update

--- Cogni/Database/Repositories/FriendRepository.cs
+++ Cogni/Database/Repositories/FriendRepository.cs
@@ -7,60 +7,78 @@ namespace Cogni.Database.Repositories
 {
     public class FriendRepository : IFriendRepository
     {
+        // todo: put it somewhere else
+        private readonly string placeholderImage = "https://yt3.googleusercontent.com/cBNnmASNGeUEDG3ij8pHF6592DTJWnwRPsrAGIql7p5P7hdw9VwQ_HJdZG9Pwjk806tQCMTbhw=s900-c-k-c0x00ffffff-no-rj";
         private readonly CogniDbContext _context;
         public FriendRepository(CogniDbContext context)
         {
             _context = context;
         }
-        public async Task<List<Friend>> GetAllUserFriends(int userId)
+        public async Task<List<FriendDto>> GetUserFriends(int userId)
         {
-           var list = await _context.Friends.Where(u => u.UserId == userId || u.FriendId == userId).ToListAsync();
-            return list;
+            var friends = await _context.Users
+                .Where(u => _context.Friends.Any(f => f.UserId == userId && f.FriendId == u.Id) && 
+                            _context.Friends.Any(f => f.UserId == u.Id && f.FriendId == userId))
+                .Select(u => new
+                {
+                    Id = u.Id,
+                    Name = u.Name,
+                    Surname = u.Surname,
+                    PicUrl = u.Avatars.Where(a => a.IsActive ?? false).Select(a => a.AvatarUrl).FirstOrDefault(),
+                    Mbti = u.IdMbtiTypeNavigation.Id
+                })
+                .ToListAsync();
+
+            return friends.Select(f => new FriendDto
+            {
+                Id = f.Id,
+                Name = f.Name,
+                Surname = f.Surname,
+                PicUrl = f.PicUrl ?? placeholderImage,
+                Mbti = f.Mbti
+            }).ToList();
+        }
+
+        public async Task Subscribe(int userId, int friendId)
+        {
+            var friend = new Friend
+            {
+                UserId = userId,
+                FriendId = friendId
+            };
+            await _context.Friends.AddAsync(friend);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Unsubscribe(int userId, int friendId)
+        {
+            var friend = await _context.Friends
+                .FirstOrDefaultAsync(f => f.UserId == userId && f.FriendId == friendId);
+            _context.Friends.Remove(friend);
+            await _context.SaveChangesAsync();
         }
 
         public async Task<List<(int id, string picUrl)>> GetFriendsPreview(int userId)
         {
-            var result = await _context.Friends
-            .Where(f => f.UserId == userId || f.FriendId == userId)
+            var friends = await _context.Users
+            .Where(u => _context.Friends.Any(f => f.UserId == userId && f.FriendId == u.Id) && 
+                        _context.Friends.Any(f => f.UserId == u.Id && f.FriendId == userId))
+            .Select(u => new
+            {
+                Id = u.Id,
+                PicUrl = u.Avatars.Where(a => a.IsActive ?? false).Select(a => a.AvatarUrl).FirstOrDefault()
+            })
             .Take(6)
             .ToListAsync();
-            List<int> list = new List<int>();
-            foreach (var f in result)
-            {
-                if (f.UserId == userId)
-                {
-                    list.Add(f.FriendId);
-                }
-                else { list.Add(f.UserId); }
-            }
-            List<(int, string)> list2 = new List<(int, string)> ();
-            List<User> users = new List<User> ();
-            foreach (var f in list)
-            {
-                var e = await _context.Users
-                     .Include(u => u.Avatars)
-                    .FirstOrDefaultAsync(u => u.Id == f);
-                users.Add(e);
-            }
-            foreach (var f in users)
-            {
-                if (f.Avatars.Any())
-                {
-                    var e = f.Avatars.FirstOrDefault(a => a.IsActive ==true);
-                    list2.Add((f.Id, e.AvatarUrl));
-                }
-                else
-                {
-                    list2.Add((f.Id, "https://yt3.googleusercontent.com/cBNnmASNGeUEDG3ij8pHF6592DTJWnwRPsrAGIql7p5P7hdw9VwQ_HJdZG9Pwjk806tQCMTbhw=s900-c-k-c0x00ffffff-no-rj"));
-                }
-            }
-            
-            return list2;
+
+            return friends.Select(f => (f.Id, f.PicUrl ?? placeholderImage)).ToList();
         }
 
         public async Task<int> GetNumOfFriends(int userId)
         {
-           var list = await _context.Friends.Where(f => f.UserId == userId || f.FriendId == userId).CountAsync();
+           var list = await _context.Friends
+                .Where(f1 => f1.UserId == userId)
+                .Where(f1 => _context.Friends.Any(f2 => f2.UserId == f1.FriendId && f2.FriendId == userId)).CountAsync();
             return list;
         }
     }
