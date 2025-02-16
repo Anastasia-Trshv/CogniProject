905ce6c8a4fd31b778031b44955ef9e9b06f63be
iv-develop <158550581+iv-develop@users.noreply.github.com>
2025-02-17 00:25:25 +0300

    friends api update

---
+++ Cogni/Models/FriendDTO.cs
@@ -0,0 +1,8 @@
+public class FriendDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string PicUrl { get; set; }
+    public int Mbti { get; set; }
+}
